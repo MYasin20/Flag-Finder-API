ad9a060e0f50ee45b1e3ff12c58299e4253d6210
MYasin <yasin.ego10@gmail.com>
2023-06-18 21:14:59 +0800

    fix bug when at page 2 then change region with search.

--- src/components/CountryLists.jsx
+++ src/components/CountryLists.jsx
@@ -14,39 +14,50 @@ const CountryLists = () => {
   const [isRegionSelected, setRegionSelected] = useState(false);
 
   const handleSearchData = (searchData) => {
-    console.log(searchData);
+    setSearchField(searchData);
+    setCurrentPage(1);
     if (!isRegionSelected) { // no region was select
       const result = data.filter(country => country.name.toLowerCase().match(searchData, 'gi'));
-      console.log(result);
       setDisplayCountriesData(result);
-      setCurrentPage(1);
-    } else {
+    } else if (isRegionSelected) {
       const result = temporaryStorageData.filter(country => country.name.toLowerCase().match(searchData, 'gi'));
       setDisplayCountriesData(result);
     }
-    setSearchField(searchData);
   }
 
   const handleRegionData = (regionData) => {
-    console.log(regionData);
     const result = data.filter(country => country.region.toLowerCase() === regionData);
-    if (regionData && searchField) {
-      const newResult = result.filter(country => country.region.toLowerCase().match(searchField, 'gi'));
-      setDisplayCountriesData(newResult);
-    } else if (regionData) {
-      setTemporaryStorageData(result);
-      setRegionSelected(true);
-      setDisplayCountriesData(result);
-      setCurrentPage(1);
-    } else if (!regionData) {
-      setRegionSelected(false);
-      setDisplayCountriesData([...data]);
+    setTemporaryStorageData(result);
+    regionData ? setRegionSelected(true) : setRegionSelected(false);
+    setCurrentPage(1);
+
+    if (result.length > 0 && searchField) { // result.length means that there is valid data to be displayed along with the searchField
+      if (temporaryStorageData === result) {
+        console.log(temporaryStorageData, '--- there is data');
+        const newResult = temporaryStorageData.filter(country => country.name.toLowerCase().match(searchField, 'gi'));
+        setDisplayCountriesData(newResult);
+      } else if (temporaryStorageData) {
+        const newResult = result.filter(country => country.name.toLowerCase().match(searchField, 'gi'));
+        setDisplayCountriesData(newResult);
+      } else if (!temporaryStorageData) {
+        console.log(temporaryStorageData, '--- there is no data');
+        const newResult = result.filter(country => country.name.toLowerCase().match(searchField, 'gi'));
+        setDisplayCountriesData(newResult);
+      }
+    } else {
+      if (regionData && !searchField) {
+        setDisplayCountriesData(result);
+      } else if (!regionData && searchField) {
+        const newResult = data.filter(country => country.name.toLowerCase().match(searchField, 'gi'));
+        setDisplayCountriesData(newResult);
+      }
     }
   }
 
   const currentCountryDisplay = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
+
     return displayCountriesData.slice(firstPageIndex, lastPageIndex);
   }, [displayCountriesData, currentPage]);
 
@@ -60,7 +71,6 @@ const CountryLists = () => {
         totalCount={displayCountriesData.length}
         pageSize={PageSize}
         onPageChange={page => setCurrentPage(page)}
-
       />
     </main>
   )
