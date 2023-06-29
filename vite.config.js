201ac24b9b615cdeb64f1a7bcff7586e6b5f78fc
MYasin <yasin.ego10@gmail.com>
2023-06-29 12:50:17 +0800

    added base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/https://github.com/MYasin20/Flag-Finder-API",
   plugins: [react()],
 })
