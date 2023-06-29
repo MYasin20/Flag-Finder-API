c862711def040513d05af12cd9a7412b3acf0e0f
MYasin <yasin.ego10@gmail.com>
2023-06-29 14:06:27 +0800

    remove gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/MYasin20/Flag-Finder-API",
   plugins: [react()],
 })
